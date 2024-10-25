86661f353535a5379b15dd6d212d11bc7885d7c5
AmoInno <amogelangmashishi12@gmail.com>
2024-10-25 16:47:21 +0200

    code more readable&corrected remaining fuel calc

--- index.js
+++ index.js
@@ -6,26 +6,28 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const intialDistance = 0; // distance (km)
+const initialFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const newDistance = intialDistance + (initialVelocity*time) //calcultes new distance
+const remainingFuel = initialFuel - (fuelBurnRate*time) //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+const calcNewVel = (acceleration, initialVelocity, time) => { 
+  return initialVelocity + (acceleration*time)
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+const newVelocity = calcNewVel(acceleration, initialVelocity, time) //calculates new velocity based on acceleration
+
+
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
