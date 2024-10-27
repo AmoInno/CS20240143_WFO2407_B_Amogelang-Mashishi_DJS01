d2b390bb47002453fa5b30adea8a2b65633f39cc
AmoInno <amogelangmashishi12@gmail.com>
2024-10-27 02:49:26 +0200

    making function robust

--- index.js
+++ index.js
@@ -14,17 +14,26 @@ const initialFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const newDistance = intialDistance + (initialVelocity*time/3600) //calcultes new distance
-const remainingFuel = initialFuel - (fuelBurnRate*time) //calculates remaining fuel
-
 // Pick up an error with how the function below is called and make it robust to such errors
 const calcNewVel = (acceleration, initialVelocity, time) => { 
+
+// Input validation
+if (typeof acceleration !== "number" || typeof initialVelocity !== "number" || typeof time !== "number"){
+  throw new TypeError ("All inputs must be a number!")
+}
+
+// Checking the range of the time
+if (time < 0){
+  throw new RangeError ("Time can never be a negative number!")
+}
+
   const finalVelocity = (initialVelocity*1000/3600) + (acceleration*time)
   return finalVelocity*3600/1000
 }
 
 const newVelocity = calcNewVel(acceleration, initialVelocity, time) //calculates new velocity based on acceleration
-
+const newDistance = intialDistance + (initialVelocity*time/3600) //calcultes new distance
+const remainingFuel = initialFuel - (fuelBurnRate*time) //calculates remaining fuel
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
